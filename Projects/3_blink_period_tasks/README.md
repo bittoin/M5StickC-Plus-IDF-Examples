85bd97ae34602e085c321d98c8597e75f534d20f
Mateus Antonio <mateustoin@gmail.com>
2022-02-07 22:46:17 -0300

    feat: Explicação do terceiro projeto

--- Projects/3_blink_period_tasks/README.md
+++ Projects/3_blink_period_tasks/README.md
@@ -1,3 +1,27 @@
 # Blink Example
 
 O objetivo desse exemplo é fazer com que o led pisque, enquanto o monitor serial espera novos valores para o período de piscada do LED. Para isso, foram utilizadas duas tasks, uma responsável por fazer o led piscar e a outra responsável por capturar o novo valor, transformar para inteiro e alterar a variável que referencia o período em que o led vai ficar ligado/desligado.
+
+## Informações do projeto
+
+Este exemplo foi retirado e modificado de uma aula sobre FreeRTOS realizada no canal oficial da Digi-Key no YouTube. Seu funcionamento consiste na execução de duas tasks: `consoleAPP()` e `toggleLed()`.
+
+### Função `consoleApp()`
+
+Essa função é responsável por esperar a entrada de dados do usuário. Quando o usuário insere algum valor no console, se for um número inteiro válido, será convertido de texto para uma variável inteira e atualizará o intervalo de tempo em que o LED pisca.
+
+### Função `toggleLed()`
+
+Essa função é responsável unicamente por ligar e desligar o LED onboard da placa, dado o intervalo de tempo inserido pelo usuário. Por padrão, intervalos começam em 100ms (1 segundo).
+
+### Fontes
+
+- [Video: Task Scheduling](https://youtu.be/95yUbClyf3E)
+- [Explicação sobre entrada de dados no console Doc oficial Espressif](https://docs.espressif.com/projects/esp-idf/en/v4.4/esp32/api-reference/peripherals/uart.html)
+
+## Projeto funcionando
+
+<div align='center'>
+    <img src="https://github.com/bittoin/M5StickC-Plus-IDF-Examples/blob/main/Images/3_blink_input_cp.gif?raw=true">
+    <p>Intervalo de piscadas alterna conforme entrada do usuário</p>
+</div>
--- Projects/3_blink_period_tasks/main/blink.c
+++ Projects/3_blink_period_tasks/main/blink.c
@@ -23,63 +23,65 @@
    or you can edit the following line and set a number here.
 */
 #define BLINK_GPIO 10
-#define app_cpu 1
+#define APP_CPU 1
 
 uint16_t blinkPeriod = 1000;
 
 void toggleLed(void *parameter){
-    while(1) {
+    gpio_reset_pin(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+    while(true) {
         /* Blink off (output low) */
-        printf("Turning off the LED\n");
         gpio_set_level(BLINK_GPIO, 0);
         vTaskDelay(blinkPeriod / portTICK_PERIOD_MS);
         /* Blink on (output high) */
-        printf("Turning on the LED\n");
         gpio_set_level(BLINK_GPIO, 1);
         vTaskDelay(blinkPeriod / portTICK_PERIOD_MS);
     }
 }
 
-void readBlinkPeriod(void *parameter){
+void consoleApp(void *parameter){
     setvbuf(stdin, NULL, _IONBF, 0);
     setvbuf(stdout, NULL, _IONBF, 0);
     ESP_ERROR_CHECK(uart_driver_install(CONFIG_ESP_CONSOLE_UART_NUM, 256, 0, 0, NULL, 0));
     esp_vfs_dev_uart_use_driver(CONFIG_ESP_CONSOLE_UART_NUM);
     esp_vfs_dev_uart_port_set_rx_line_endings(CONFIG_ESP_CONSOLE_UART_NUM, ESP_LINE_ENDINGS_CR);
     esp_vfs_dev_uart_port_set_tx_line_endings(CONFIG_ESP_CONSOLE_UART_NUM, ESP_LINE_ENDINGS_CRLF);
-    char chr[10];
+    char chr[9];
+
     while(1){
-        printf("Enter period for led : ");
-        scanf("%9s", chr);
-        printf("\nData entered : %s\n", chr);
-        int period = atoi(chr);
-        blinkPeriod = period;
+        printf("Enter value (0 - 65536) in ms to blink: \n");
+        gets(chr);
+        uint16_t newPeriod = atoi(chr);
+        if (newPeriod != 0){    // Valid number
+            blinkPeriod = newPeriod;
+            printf("Value: %d(ms)\n", blinkPeriod);
+        }else{                  // Invalid number
+            printf("Number not valid!\n");
+        }
     }
 }
 
-void app_main(void)
-{
-    gpio_reset_pin(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-
+void app_main(void){
     xTaskCreatePinnedToCore(
-        readBlinkPeriod,
-        "Toggle LED",
+        consoleApp,
+        "Console app",
         4096,
         NULL,
         1,
         NULL,
-        app_cpu
+        APP_CPU
     );
 
     xTaskCreatePinnedToCore(
         toggleLed,
         "Toggle LED",
-        1024,
+        4096,
         NULL,
         1,
         NULL,
-        app_cpu
+        APP_CPU
     );
 }
