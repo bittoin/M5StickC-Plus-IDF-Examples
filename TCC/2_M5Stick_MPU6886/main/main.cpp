a9b42514b5fb3359913f43f4635f40dfe897635e
Mateus Antonio <mateustoin@gmail.com>
2022-09-16 08:24:49 -0300

    Adding working example with MPU6886 and M5StickC

---
+++ TCC/2_M5Stick_MPU6886/main/main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <mpu6886.h>
+#include <freertos/FreeRTOS.h>
+
+float aX, aY, aZ;
+i2c_port_t port = I2C_NUM_0;
+
+extern "C" void app_main(void)
+{
+    aX = 0.0;
+    aY = 0.0;
+    aZ = 0.0;
+
+    mpu6886_init(&port);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+    printf( "MPU6886 main: Init MPU6886 finished\n\n" );
+    while(1){
+        mpu6886_accel_data_get(&aX, &aY, &aZ);
+        printf("MPU6886: aX: %f, aY: %f, aZ: %f\n", aX, aY, aZ);
+        vTaskDelay(50 / portTICK_PERIOD_MS);
+    }
+}
