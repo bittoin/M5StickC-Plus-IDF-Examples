6d9569ae2ce1482b3cc2e39cb1e4c1b716c58140
Mateus Antonio <mateustoin@gmail.com>
2022-09-16 22:39:49 -0300

    Changing code to match Serial Plotter pattern

--- TCC/2_M5Stick_MPU6886/main/main.cpp
+++ TCC/2_M5Stick_MPU6886/main/main.cpp
@@ -13,10 +13,11 @@ extern "C" void app_main(void)
 
     mpu6886_init(&port);
     vTaskDelay(2000 / portTICK_PERIOD_MS);
-    printf( "MPU6886 main: Init MPU6886 finished\n\n" );
+    //printf( "MPU6886 main: Init MPU6886 finished\n\n" );
     while(1){
         mpu6886_accel_data_get(&aX, &aY, &aZ);
-        printf("MPU6886: aX: %f, aY: %f, aZ: %f\n", aX, aY, aZ);
+        printf("ax:%f,aY:%f,aZ:%f\n", aX, aY, aZ);
+        // printf("MPU6886: aX: %f, aY: %f, aZ: %f\n", aX, aY, aZ);
         vTaskDelay(50 / portTICK_PERIOD_MS);
     }
 }
