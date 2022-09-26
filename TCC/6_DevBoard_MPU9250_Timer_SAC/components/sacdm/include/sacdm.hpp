16ac52efa7669702adcca5493884514f78aafe12
Mateus Antonio <mateustoin@gmail.com>
2022-09-26 00:16:57 -0300

    Example with sac-dm and timer working on esp_timer

--- TCC/6_DevBoard_MPU9250_Timer_SAC/components/sacdm/include/sacdm.hpp
+++ TCC/6_DevBoard_MPU9250_Timer_SAC/components/sacdm/include/sacdm.hpp
@@ -7,20 +7,56 @@ extern "C"
 
 #include "driver/gpio.h"
 #include "driver/i2c.h"
+#include "freertos/timers.h"
+
+#include "I2Cbus.hpp"
+#include "MPU.hpp"
 
 #include "esp_err.h"
 #include "esp_log.h"
 
-// #define SAMPLE_SIZE 500
+class SAC_DM {
+    public:
+        // SAC-DM Variable declaration
+        static long lastMsg, loopTimer;
+        static int value, readings, peaks_x, peaks_y, peaks_z;
+        static float signals_x[3];
+        static float signals_y[3];
+        static float signals_z[3];
+        static double rho_x, rho_y, rho_z;
+        static int count;
 
-const int SAMPLE_SIZE = 500;
-const float threshold = 1.20;
+        static MPU_t MPU;  // create a default MPU object
 
-static constexpr gpio_num_t SDA = GPIO_NUM_21;
-static constexpr gpio_num_t SCL = GPIO_NUM_22;
-static constexpr uint32_t CLOCK_SPEED = 400000;  // range from 100 KHz ~ 400Hz
+        // Reading sensor data
+        static mpud::raw_axes_t accelRaw;   // x, y, z axes as int16
+        static mpud::float_axes_t accelG;   // accel axes in (g) gravity format
 
-void init_sacdm();
+
+        static SAC_DM& GetInstance() {
+            // Allocate with `new` in case Singleton is not trivially destructible.
+            static SAC_DM* sac_dm = new SAC_DM();
+            return *sac_dm;
+        }
+        SAC_DM();
+        void initialize_sac_dm();
+        static void calculate();
+
+    private:
+    //SAC_DM() = default;
+    // Delete copy/move so extra instances can't be created/moved.
+    SAC_DM(const SAC_DM&) = delete;
+    SAC_DM& operator=(const SAC_DM&) = delete;
+    SAC_DM(SAC_DM&&) = delete;
+    SAC_DM& operator=(SAC_DM&&) = delete;
+
+    static constexpr const int SAMPLE_SIZE = 500;
+    static constexpr const float threshold = 1.20;
+
+    static constexpr gpio_num_t SDA = GPIO_NUM_21;
+    static constexpr gpio_num_t SCL = GPIO_NUM_22;
+    static constexpr uint32_t CLOCK_SPEED = 400000;  // range from 100 KHz ~ 400Hz
+};
 
 #ifdef _cplusplus
 }
--- TCC/6_DevBoard_MPU9250_Timer_SAC/main/main.cpp
+++ TCC/6_DevBoard_MPU9250_Timer_SAC/main/main.cpp
@@ -4,6 +4,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/timers.h"
+#include "esp_timer.h"
 
 #include "sacdm.hpp"
 
@@ -11,11 +12,22 @@ extern "C" void app_main();
 
 static const char* TAG = "MAIN";
 
+void call_calculate_timer(void *args) {
+    SAC_DM::calculate();
+}
+
 void app_main() {
     ESP_LOGI(TAG, "$ MPU Driver Example: MPU-I2C\n");
     fflush(stdout);
-    
-    init_sacdm();
-    // TimerHandle_t xTime = xTimerCreate("Timer sac-dm", (2/portTICK_PERIOD_MS), pdTRUE, NULL, measure_sac_dm);
+    SAC_DM sac;
+    sac.initialize_sac_dm();
+    // TimerHandle_t xTime = xTimerCreate("Timer sac-dm", ( 10 / portTICK_PERIOD_MS ), pdTRUE, NULL, call_calculate_timer);
     // xTimerStart(xTime, 0);
+    const esp_timer_create_args_t esp_timer_create_args = {
+        .callback = call_calculate_timer,
+        .name = "SAC Timer"
+    };
+    esp_timer_handle_t esp_timer_handle;
+    esp_timer_create(&esp_timer_create_args, &esp_timer_handle);
+    esp_timer_start_periodic(esp_timer_handle, 2*1000);
 }
