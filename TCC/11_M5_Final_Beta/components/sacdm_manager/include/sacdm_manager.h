500f5892ee5725ec55bf0e9125a0b716af8b13b8
Mateus Antonio <mateustoin@gmail.com>
2022-10-13 21:15:54 -0300

    Planning new libs inside sacdm_manager

--- TCC/11_M5_Final_Beta/components/sacdm_manager/include/sacdm_manager.h
+++ TCC/11_M5_Final_Beta/components/sacdm_manager/include/sacdm_manager.h
@@ -1,1 +1,25 @@
+#pragma once
+
+#include <stdio.h>
+
+/*
+    ************************************
+    Definição de variáveis para o SAC-DM
+    ************************************
+*/
+#define SAMPLE_SIZE 500
+
+static float threshold = 1.20;
+static long lastMsg = 0, loopTimer = 0;
+static int value = 0, readings = 0, peaks_x = 0, peaks_y = 0, peaks_z = 0;
+static double rho_x, rho_y, rho_z;
+
+static int16_t signals_x[3] = {0, 0, 0};
+static int16_t signals_y[3] = {0, 0, 0};
+static int16_t signals_z[3] = {0, 0, 0};
+
+static int16_t accX = 0;
+static int16_t accY = 0;
+static int16_t accZ = 0;
+
 void func(void);
