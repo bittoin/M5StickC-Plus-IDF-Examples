0f45e7a0f593326d468c2fe6d1c68369cf96c30b
Mateus Antonio <mateustoin@gmail.com>
2022-10-16 07:47:24 -0300

    Button crontrolling M5 Led

--- Generic_Projects/11_button_events/main/main.c
+++ Generic_Projects/11_button_events/main/main.c
@@ -10,16 +10,24 @@
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/gpio.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 #include "esp_log.h"
 #include "iot_button.h"
 
+#define M5_BUTTON_A 37
+#define M5_BUTTON_B 39
+#define M5_LED GPIO_NUM_10
+#define M5_BUZZER GPIO_NUM_2
+
+static int led_state = 1;
+
 // create gpio button
 button_config_t gpio_btn_cfg = {
     .type = BUTTON_TYPE_GPIO,
     .gpio_button_config = {
-        .gpio_num = 37,
+        .gpio_num = M5_BUTTON_A,
         .active_level = 0,
     },
 };
@@ -29,6 +37,8 @@ const static char* TAG = "button_example";
 static void button_single_click_cb(void *arg)
 {
     ESP_LOGI(TAG, "BUTTON_SINGLE_CLICK");
+    gpio_set_level(M5_LED, led_state);
+    led_state = !led_state;
 }
 
 static void button_double_click_cb(void *arg)
@@ -58,14 +68,19 @@ static void button_long_press_hold_cb(void *arg)
 
 void app_main(void)
 {
+    // gpio_reset_pin(M5_LED);
+    gpio_pad_select_gpio(M5_LED);
+    gpio_set_direction(M5_LED, GPIO_MODE_OUTPUT);
+    gpio_set_level(M5_LED, 0);
+    
     button_handle_t gpio_btn = iot_button_create(&gpio_btn_cfg);
     if(NULL == gpio_btn) {
         ESP_LOGE(TAG, "Button create failed");
     }
     iot_button_register_cb(gpio_btn, BUTTON_SINGLE_CLICK, button_single_click_cb, NULL);
     iot_button_register_cb(gpio_btn, BUTTON_DOUBLE_CLICK, button_double_click_cb, NULL);
-    iot_button_register_cb(gpio_btn, BUTTON_PRESS_DOWN, button_pressed_cb, NULL);
-    iot_button_register_cb(gpio_btn, BUTTON_PRESS_UP, button_released_cb, NULL);
-    iot_button_register_cb(gpio_btn, BUTTON_LONG_PRESS_START, button_long_press_cb, NULL);
-    iot_button_register_cb(gpio_btn, BUTTON_LONG_PRESS_HOLD, button_long_press_hold_cb, NULL);
+    // iot_button_register_cb(gpio_btn, BUTTON_PRESS_DOWN, button_pressed_cb, NULL);
+    // iot_button_register_cb(gpio_btn, BUTTON_PRESS_UP, button_released_cb, NULL);
+    // iot_button_register_cb(gpio_btn, BUTTON_LONG_PRESS_START, button_long_press_cb, NULL);
+    // iot_button_register_cb(gpio_btn, BUTTON_LONG_PRESS_HOLD, button_long_press_hold_cb, NULL);
 }
